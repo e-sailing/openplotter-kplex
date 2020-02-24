65662a604b57a96fb30a13f0afd972b068293f85
e-sailing <e.minus.sailing@gmail.com>
2020-02-24 23:28:32 +0100

    init

--- README.md
+++ README.md
@@ -1,1 +1,54 @@
-openplotter-kplex
+## openplotter-kplex
+
+OpenPlotter app to manage kplex devices
+
+### Installing
+
+#### For production
+
+Install [openplotter-settings](https://github.com/openplotter/openplotter-settings) for **production** and install this app from *OpenPlotter Apps* tab.
+
+#### For development
+
+Install [openplotter-settings](https://github.com/openplotter/openplotter-settings) for **development**.
+
+Install dependencies:
+Download kplex from http://www.stripydog.com/kplex/download.html
+
+`dpkg -i ./kplex_1.x-x_xxx.deb`
+
+Clone the repository:
+
+`git clone https://github.com/openplotter/openplotter-kplex`
+
+Make your changes and create the package:
+
+```
+cd openplotter-kplex
+dpkg-buildpackage -b
+```
+
+Install the package:
+
+```
+cd ..
+sudo dpkg -i openplotter-kplex_x.x.x-xxx_all.deb
+```
+
+Run post-installation script:
+
+`sudo kplexPostInstall`
+
+Run:
+
+`openplotter-kplex`
+
+Make your changes and repeat package, installation and post-installation steps to test. Pull request your changes to github and we will check and add them to the next version of the [Debian package](https://cloudsmith.io/~openplotter/repos/openplotter/packages/).
+
+### Documentation
+
+https://openplotter.readthedocs.io
+
+### Support
+
+http://forum.openmarine.net/forumdisplay.php?fid=1
---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+# This file is part of Openplotter.
+# Copyright (C) 2019 by sailoog <https://github.com/sailoog/openplotter-kplex>
+#                     e-sailing <https://github.com/e-sailing/openplotter-kplex>
+# Openplotter is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# any later version.
+# Openplotter is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Openplotter. If not, see <http://www.gnu.org/licenses/>.
+
+from setuptools import setup
+from openplotterKplex import version
+
+setup (
+	name = 'openplotterKplex',
+	version = version.version,
+	description = 'OpenPlotter app to manage serial devices',
+	license = 'GPLv3',
+	author="e-sailing",
+	author_email='e.minus.sailing@gmail.com',
+	url='https://github.com/openplotter/openplotter-kplex',
+	packages=['openplotterKplex'],
+	classifiers = ['Natural Language :: English',
+	'Operating System :: POSIX :: Linux',
+	'Programming Language :: Python :: 3'],
+	include_package_data=True,
+	entry_points={'console_scripts': ['openplotter-kplex=openplotterKplex.openplotterKplex:main', 'kplexPostInstall=openplotterKplex.kplexPostInstall:main', 'kplexPreUninstall=openplotterKplex.kplexPreUninstall:main']},
+	data_files=[('share/applications', ['openplotterKplex/data/openplotter-kplex.desktop']),('share/pixmaps', ['openplotterKplex/data/kplex.png']),],
+	)
